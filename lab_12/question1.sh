71322a0589332580b41ecc65e4addd98c7c336c0
reethikaa-08 <rithika.rithu0807@gmail.com>
2026-09-16 10:44:09 +0530

    Resolve merge conflicts

--- lab_12/question1.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for (( i=0 ;i<=10 ;i++ ))
-do
-	echo "Print values of i from 1 to 10 : " $i
-
-done
--- lab_12/question3.sh
+++
@@ -1,8 +0,0 @@
-echo "Enter numbers"
-read num
- i=1
- until [ $i -gt 15 ]
- do
-	 echo " $num x $i = $((num * i))"
-	 i=$((i+1))
- done
